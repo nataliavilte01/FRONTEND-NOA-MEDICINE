a0861db01744e4f220a5f53de25a73e8b2524eea
nataliavilte01 <natyvilte03@gmail.com>
2025-02-21 18:52:34 -0300

    Implementacion de medico: formulario de medico y lista de medico.

--- src/app/global/global/global.module.ts
+++ src/app/global/global/global.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
 @NgModule({
   declarations: [],
   imports: [
@@ -20,17 +21,21 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatSelectModule,
+    MatDatepickerModule,
   ],
-  exports:[
-  MatGridListModule,
-  MatButtonModule,
-  MatIconModule,
-  MatTableModule,
-  MatDialogModule,
-  MatFormFieldModule,
- MatInputModule,
- MatProgressBarModule
-]
+  exports: [
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatDatepickerModule,
+  ]
 })
 export class GlobalModule { }
